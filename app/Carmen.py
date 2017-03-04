3c499ce8048af52e1ae50d7c32c06f301d6ddcbd
Carmen Overton <carmen@Carmens-MacBook-Pro.local>
2017-03-04 10:05:47 -0800

    adjDirection and checkBody

--- app/Carmen.py
+++ app/Carmen.py
@@ -11,6 +11,22 @@ BODIES = 4
 TAILS = 5
 EMPTY = 0
 
+def adjDirection(headPos,bodyPos):
+    if(headPos[0]-bodyPos[0] == 0): #if no difference in x direction
+        if(headPos[1]-bodypos[1] < 0): #if body is  below head
+            return 'down'; #down is to be removed from directions list
+        else:
+            return 'up'; #up is to be removed from directions list
+    else: #if there is a difference in the x direction
+        if(headPos[0] - bodyPos[0] < 0): #if body is to the right of head
+            return 'right'; #right is to be removed from directions list
+        else:
+            return 'left';
+
+
+def checkBody(grid, directions):
+    directions.remove(Dir(ourSnake[0],ourSnake[1]))
+
 
 def creategrid(data):
     grid = [[0 for col in range(data['width'])] for row in range(data['height'])]
