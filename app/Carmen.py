bd0f5d1edc8916156355870bedc2a07b436dab7b
Carmen Overton <carmen@Carmens-MacBook-Pro.local>
2017-03-04 10:37:09 -0800

    adjCoords function added

--- app/Carmen.py
+++ app/Carmen.py
@@ -17,8 +17,28 @@ def adjDirection(headPos,bodyPos):
             return 'left'; #left is to be removed from directions list
 
 
+#THIS FUNCTION RETURNS A LIST OF COORDINATES COORESPONDING TO DIRECTIONS
+#ORDER: right, left, down, up.
+def adjCoords(ourSnake): #returns a list of 4 coordinates to check for extra snake body
+    headPos = ourSnake[0];
+    x = headPos[0];
+    y = headPos[1];
+    
+    return [[x+1,y],[x-1,y],[x,y+1],[x,y-1]]; #right, left, down, up
+
+
 def checkBody(directions):
     directions.remove(adjDirection(ourSnake[0],ourSnake[1]))
+    adjCoords = adjCoords(ourSnake);
+    i = 0;
+    if (adjCoords[0] in ourSnake):
+        directions.remove('right');
+    if (adjCoords[1] in ourSnake):
+        directions.remove('left');
+    if (adjCoords[2] in ourSnake):
+        directions.remove('down');
+    if (adjCoords[3] in ourSnake):
+        directions.remove('up');
 
 def firstCheck(directions):
     checkBody();
